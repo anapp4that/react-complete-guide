e850ee7ad3013370dbf65bbca88df59f1c842f46
Shelby Anderson <shelby.anderson@tyson.com>
2019-07-25 09:22:24 -0500

    Created Auxillary.js, a Higher order component, to wrap our content we were returning in an array.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react'
 import classes from './Person.module.css'
-
+import Aux from '../../../hoc/Auxiliary.js'
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
-        return [
-            //if you dont technically need a div from an html
-            //or css point of view you don't technically have to use
-            //a wrapping div
-            //< div className={classes.Person} >
-            <p key='i1' onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
-            <p key='i2'>{this.props.children}</p>,
-            <input key='i3' type="text" onChange={this.props.changed} value={this.props.name} />
-            //</div >
-
-        ]
+        return (
+            <Aux className={classes.Person}>
+                <p key='i1' onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p key='i2'>{this.props.children}</p>
+                <input key='i3' type="text" onChange={this.props.changed} value={this.props.name} />
+            </Aux>
+        )
     }
 }
 
---
+++ src/hoc/Auxiliary.js
@@ -0,0 +1,5 @@
+//This just returns the input wrapped between
+//the opening and closing tags
+const Auxiliary = (props) => props.children
+
+export default Auxiliary
