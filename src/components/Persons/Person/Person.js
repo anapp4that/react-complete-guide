6d28372d4a326f9fb4bebeca887893b3964c1360
Shelby Anderson <shelby.anderson@tyson.com>
2019-07-25 09:24:45 -0500

    Switched our our custom Auxillary wrapper for the built in Fragment wrapper

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import classes from './Person.module.css'
-import Aux from '../../../hoc/Auxiliary.js'
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
         return (
-            <Aux className={classes.Person}>
+            <Fragment>
                 <p key='i1' onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p key='i2'>{this.props.children}</p>
                 <input key='i3' type="text" onChange={this.props.changed} value={this.props.name} />
-            </Aux>
+            </Fragment>
         )
     }
 }
