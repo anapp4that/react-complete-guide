32f50afb937db447626be50d8ce8a88838ae212b
Shelby Anderson <shelby.anderson@tyson.com>
2019-07-25 08:46:10 -0500

    Made Persons.js into a pure component

--- src/componenets/Persons/Persons.js
+++ src/componenets/Persons/Persons.js
@@ -1,53 +1,58 @@
-import React, { Component } from 'react'
+//extending pure component gives you a component that
+//implements shouldComponentUpdate with a check of every 
+//prop for that component
+import React, { PureComponent } from 'react'
 import Person from './Person/Person'
 
 //With ES6, if using the arrow function, you can omit
 //the return in the function body whats in () after
 //the => gets returned automatically
-class Persons extends Component {
+class Persons extends PureComponent {
 
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log('[Persons.js] shouldComponentUpdate')
-        //if the persons prop doest change we dont rerender
-        if (nextProps.persons !== this.props.persons) {
-            return true;
+        // shouldComponentUpdate(nextProps, nextState) {
+    //     console.log('[Persons.js] shouldComponentUpdate')
+          //     //if the persons prop doest change we dont rerender
+        //     if (nextProps.persons !== this.props.persons
+        //         || nextProps.changed !== this.props.changed
+        //         || nextProps.click !== this.props.click) {
+        //         return true;
+        //     }
+        //     else {
+        //         return false;
+        //     }
+        // }
+
+        getSnapshotBeforeUpdate(prevProps, prevState) {
+            console.log('[Persons.js] getSnapshotBeforeUpdate')
+            return { message: 'Snapshot!' }
         }
-        else {
-            return false;
+
+        componentDidUpdate(prevProps, prevState, snapshot) {
+            console.log('[Persons.js] componentDidUpdate')
+            console.log(snapshot);
+        }
+
+        componentWillUnmount() {
+            console.log('[Perons.js componentWillUnmount');
+
+        }
+
+        render() {
+            console.log('[Persons.js] rendering...')
+            return (
+                this.props.persons.map((person, index) => {
+                    return (
+                        <Person
+                            name={person.name}
+                            age={person.age}
+                            click={() => this.props.clicked(index)}
+                            changed={(event) => this.props.changed(event, person.id)}
+                            key={person.id}
+                        />
+                    )
+                })
+            )
         }
     }
 
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        console.log('[Persons.js] getSnapshotBeforeUpdate')
-        return { message: 'Snapshot!' }
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('[Persons.js] componentDidUpdate')
-        console.log(snapshot);
-    }
-
-    componentWillUnmount() {
-        console.log('[Perons.js componentWillUnmount');
-
-    }
-
-    render() {
-        console.log('[Persons.js] rendering...')
-        return (
-            this.props.persons.map((person, index) => {
-                return (
-                    <Person
-                        name={person.name}
-                        age={person.age}
-                        click={() => this.props.clicked(index)}
-                        changed={(event) => this.props.changed(event, person.id)}
-                        key={person.id}
-                    />
-                )
-            })
-        )
-    }
-}
-
-export default Persons
+    export default Persons
