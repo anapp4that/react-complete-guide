9bc8da86fc3ee4929de3b69f8c9a85e086cc98ec
Shelby Anderson <shelby.anderson@tyson.com>
2019-07-30 09:48:55 -0500

    Added logo to our navigation bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '../src/components/Layout/Layout'
 import BurgerBuilder from '../src/containers/BurgerBuilder/BurgerBuilder'
-import BuildControls from '../src/components/Burger/BuildControls/BuildControls'
 
 function App() {
   return (
