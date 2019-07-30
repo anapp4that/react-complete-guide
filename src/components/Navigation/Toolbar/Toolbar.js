3b6fe66efc4af5034eb1b8fda75ec62828346197
Shelby Anderson <shelby.anderson@tyson.com>
2019-07-30 10:49:59 -0500

    Cleaned up the navigation buttons in the side drawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,20 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import Logo from '../../Logo/Logo';
-import Items from '../Items/Items';
+import Logo from "../../Logo/Logo";
+import Items from "../Items/Items";
 
-import classes from './Toolbar.module.css';
+import classes from "./Toolbar.module.css";
 
-const Toolbar = (props) => (
-    <header className={classes.Toolbar}>
-        <div>Menu</div>
-        <div className={classes.Logo}>
-            <Logo />
-        </div>
-        <nav>
-            <Items />
-        </nav>
-    </header>
-);
+const Toolbar = props =>
+  <header className={classes.Toolbar}>
+    <div>Menu</div>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <Items />
+    </nav>
+  </header>;
 
 export default Toolbar;
