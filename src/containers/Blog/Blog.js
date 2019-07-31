1404e6d3fbf379d7344a4b952fb5f9c4ab53bed8
Shelby Anderson <shelby.anderson@tyson.com>
2019-07-31 12:47:15 -0500

    Grabbed data from a 'server' and dynamically added it to the page

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,21 +7,28 @@ import NewPost from "../../components/NewPost/NewPost";
 import classes from "./Blog.module.css";
 
 class Blog extends Component {
+    state = {
+        posts: []
+    };
+
     componentDidMount() {
         axios
             .get("https://jsonplaceholder.typicode.com/posts")
             .then(response => {
+                this.setState({ posts: response.data });
                 console.log(response);
             });
     }
 
     render() {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} />;
+        });
+
         return (
             <div>
                 <section className={classes.Posts}>
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
